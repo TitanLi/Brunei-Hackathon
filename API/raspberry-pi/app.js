4e48aaeac90ab678a4a93d2e6edcd6479a117ed8
TitanLi <lisheng0706@gmail.com>
2020-02-10 20:42:36 +0800

    Add reconnection

--- API/raspberry-pi/app.js
+++ API/raspberry-pi/app.js
@@ -3,46 +3,51 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const SerialPort = require("serialport");
 const Readline = SerialPort.parsers.Readline;
-var port;
-var sensorData = {};
+let port;
+let sensorData = {};
 function connect() {
-    port = new SerialPort('/dev/tty.usbmodem141301', {
+    port = new SerialPort('/dev/ttyACM0', {
         baudRate: 9600
     });
 
-    const parser = new Readline();
-    port.pipe(parser);
-    // parser.on('data', console.log);
+    const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+
+    parser.on('data', function (data) {
+        console.log(data);
+        try {
+            sensorData = JSON.parse(data);
+        } catch (error) {
+            console.log('JSON error');
+        }
+    });
+
     port.on('open', function () {
         console.log('port open')
-        parser.on('data', function (data) {
-            console.log(JSON.parse(data));
-            sensorData = JSON.parse(data);
-        });
     });
 
     port.on('close', function (err) {
-        console.log('close')
+        console.log('close');
+        port.close();
     });
 }
 connect();
 setInterval(() => {
     port.get(function (err) {
         if (err) {
-            console.log(err);
+            console.log('Port is not open');
             sensorData = {};
             connect();
         }
     });
-}, 1000);
+}, 4000);
 const app = express();
 app.use(bodyParser.json());
-app.use(cors()); 
+app.use(cors());
 
 app.get('/query', (req, res) => {
     let query = req.query;
     const data = {
-        deviceInfo:sensorData
+        deviceInfo: sensorData
     };
     res.json(data);
 });
